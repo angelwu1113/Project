02817b186f2b40665f16a0a27865e947ea8f2124
admin <admin@admindeMacBook-Pro.local>
2021-05-05 18:50:58 -0700

    Library added

---
+++ Assets的副本/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerScript : MonoBehaviour
+{
+    public TextMeshProUGUI moveText;
+    public TextMeshProUGUI jumpText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveText.enabled = true;
+        jumpText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Check"){
+            moveText.enabled = false;
+            jumpText.enabled = true;
+        }
+        else if(other.tag == "Check2"){
+            jumpText.enabled = false;
+        }
+    }
+}
