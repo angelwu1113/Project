2b4a81ee5f2110007c588e8db49606a0fca3d1ad
admin <admin@admindeMacBook-Pro.local>
2021-06-02 18:50:53 -0700

    Dialogue

---
+++ Assets的副本/Scripts/DialogUIManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogUIManager : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject mainText;
+    public GameObject responseTab;
+    public GameObject continueText;
+    public List<Button> responsesHolder;
+    private DialogueBranch branch;
+
+    [SerializeField]
+    private int currentIndex = 0;
+    [SerializeField]
+    private int responses = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactiveDialogue();
+    }
+
+    public void ActiveDialogue()
+    {
+        background.SetActive(true);
+        mainText.SetActive(true);
+        continueText.SetActive(true);
+        responseTab.SetActive(true);
+        foreach (Button response in responsesHolder)
+        {
+            response.gameObject.SetActive(false);
+        }
+    }
+
+    public void DeactiveDialogue()
+    {
+        background.SetActive(false);
+        mainText.SetActive(false);
+        continueText.SetActive(false);
+        responseTab.SetActive(false);
+        foreach (Button response in responsesHolder)
+        {
+            response.gameObject.SetActive(false);
+        }
+    }
+
+    public void NextBranch(int branchSelect)
+    {
+        // Add ReciveDialogueBranch with newBranch being next branch
+        ReciveDialogueBranch(branch.ResponseOption[branchSelect].nextBranch);
+        ActiveDialogue();
+        NextDialogue();
+    }
+
+    public void RecieveDialogueBranch(DialogueBranch newBranch)
+    {
+        this.branch = newBranch;
+        responses = Mathf.Clamp(branch.ResponseOption.Count,0,3);
+        currentIndex = 0;
+    }
+
+    public void NextDialogue()
+    {
+        // If at end of branch
+        if (currentIndex >= branch.DialogueLines.Count)
+        {
+            // No response -> End dialogue
+            if (responses == 0)
+            {
+                DeactiveDialogue();
+            }
+            else
+            {
+                // Responses -> Activate Buttons
+                continueText.SetActive(false);
+                for (int i = 0; i < responses; i++)
+                {
+                    if (i >= 3)
+                    {
+                        break;
+                    }
+
+                    // Activate the button and place response text
+                    responsesHolder[i].gameObject.SetActive(true);
+                    responsesHolder[i].GetComponentInChildren<TextMeshProUGUI>().text = branch.ResponseOption[i].text;
+                }
+            }
+        }
+        // There is still more text to show
+        else
+        {
+            mainText.GetComponent<TextMeshProUGUI>().text = branch.DialogueLines[currentIndex];
+            continueText.SetActive(true);
+            currentIndex++;
+        }
+    }
+
+
+}
---
+++ Assets的副本/Scripts/DialoguePoint.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialoguePoint : MonoBehaviour
+{
+    public string info;
+    public TextMeshProUGUI popup;
+    public DialogUIManager dialogueMenu;
+    public DialogueBranch startBranch;
+
+    private bool inDialogueZone;
+    private bool dialoguePlaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dialogueMenu == null)
+        {
+            dialogueMenu = GameObject.FindGameObjectWithTag("DIM").GetComponent<DialogUIManager>();
+        }
+        if (popup == null)
+        {
+            popup = GameObject.FindGameObjectWithTag("DIM").GetComponentInChildren<TextMeshProUGUI>();
+            popup.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (dialoguePlaying)
+            {
+                dialogueMenu.NextDialogue();
+            }
+            else if (inDialogueZone)
+            {
+                inDialogueZone = false;
+                popup.gameObject.SetActive(false);
+                dialogueMenu.ActiveDialogue();
+                dialogueMenu.NextDialogue();
+                dialoguePlaying = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        dialogueMenu.RecieveDialogueBranch(startBranch); // Add start branch here
+        popup.gameObject.SetActive(true);
+        popup.text = "Press [E] to " + info;
+        inDialogueZone = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inDialogueZone = false;
+        popup.gameObject.SetActive(false);
+        dialoguePlaying = false;
+        dialogueMenu.DeactiveDialogue();
+    }
+}
