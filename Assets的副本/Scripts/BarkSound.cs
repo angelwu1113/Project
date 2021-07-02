587b2fc96a2b3e8a9c76aed2efedea7e7038c5ad
angelwu1113 <70672779+angelwu1113@users.noreply.github.com>
2021-07-02 09:28:18 +0800

    Free Royalty Music

---
+++ Assets的副本/Scripts/BarkSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarkSound : MonoBehaviour
+{
+    private AudioSource myAudioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter()
+    {
+        myAudioSource.Play();
+    }
+}
