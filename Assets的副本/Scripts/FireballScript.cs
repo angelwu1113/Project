9f4ea806f1e59bf089c665980475650665f29fbe
angelwu1113 <70672779+angelwu1113@users.noreply.github.com>
2021-07-23 09:24:31 +0800

    Fireball

--- Assets的副本/Scripts/FireballScript.cs
+++ Assets的副本/Scripts/FireballScript.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class FireballScript : MonoBehaviour
 {
     public GameObject prefab;
+    private GameObject holder;
+    public int waitTime = 3;
     private int posX;
     private int posZ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        holder = GameObject.Find("FireballHolder");
+        StartCoroutine(Fire());
     }
 
     // Update is called once per frame
@@ -19,11 +22,18 @@ public class FireballScript : MonoBehaviour
     {
         // z = -4, 4
         // x = -5, 5
-        for (int i = 0; i <= 9; i++)
-        {
-            posX = Random.Range(-5f, 5f);
-            posZ = Random.Range(-4f, 4f);
-            Instantiate(prefab)
+        
+
+    }
+
+    IEnumerator Fire(){
+        for(int i = 1; i <=25; i++){
+            posX = Random.Range(-40, 40);
+            posZ = Random.Range(-40, 40);
+            GameObject clone = Instantiate(prefab, new Vector3(posX, 50, posZ), Quaternion.identity);
+            clone.transform.SetParent(holder.transform);
         }
+        
+        yield return new WaitForSeconds(waitTime);
     }
 }
